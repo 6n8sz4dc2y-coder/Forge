c175e2d968f521b84f1b39435a5b84d4af094c5a
6n8sz4dc2y-coder <6n8sz4dc2y@privaterelay.appleid.com>
2026-07-04 09:50:40 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='forge-prs-v02';
-const FILES=['./','./index.html','./app.js','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
