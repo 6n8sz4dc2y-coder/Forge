5c93b57874d91ea2edca95e41db0ecdf63056a05
6n8sz4dc2y-coder <6n8sz4dc2y@privaterelay.appleid.com>
2026-07-27 07:20:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='strengthos-v3.1-custom-dashboard-layout';
+const CACHE='strengthos-v3.1.1.1-dashboard-ui-fix';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))) });
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
